7ab12bedec463471753de1a1c775dd10b2df29b6
Oleksii LEVENETS <oleksii.levenets@icloud.com>
2023-03-12 18:27:58 +0200

    v2

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    # "django.contrib.staticfiles",
     # Django contrib sites
     "django.contrib.sites",
     # My apps:
