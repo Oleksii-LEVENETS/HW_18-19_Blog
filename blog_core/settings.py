8f037e16e55a930b825725d0a21a949f83aa4a5c
Oleksii LEVENETS <64700959+Oleksii-LEVENETS@users.noreply.github.com>
2023-03-13 10:07:19 +0200

    Update settings.py

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-=8^f1+pmts+x(dr6dq^-4+8p!849$t&9gw(a8-p@p=lf492sbi")
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-=8^f1+pmts+x(dr6dq^-4+8p!849$t&9gw(a8-p@p=lf492sbi")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
